801a6e2446d30543dbf96aef9fd1235b605c57fb
Ryan Dimick <rdimick@smatechnologies.com>
2022-01-15 13:07:13 -0600

    Fixed links to proper REST API documentation

--- sidebars.js
+++ sidebars.js
@@ -610,7 +610,7 @@ module.exports = {
     {
       type: "link",
       label: "REST API Docs",
-      href: "https://help.smatechnologies.com/opcon/core/api/21-0.html",
+      href: "https://help.smatechnologies.com/opcon/core/api/21-1.html",
     },
   ],
 };
