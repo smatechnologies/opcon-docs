de77277a7b693daa3f322ef1bdf830f100d588c2
Kalyan Kanuru <kkanuru@kkanurumac.local>
2023-03-31 14:37:32 -0500

    added release notes for Opcon 22.2.0

--- sidebars.js
+++ sidebars.js
@@ -746,7 +746,7 @@ module.exports = {
     {
       type: "link",
       label: "REST API Docs",
-      href: "https://help.smatechnologies.com/opcon/core/api/22-1.html",
+      href: "https://help.smatechnologies.com/opcon/core/api/22-2.html",
     },
   ],
 };
