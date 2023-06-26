8a20608d0bc412499de7d40beebb033b463a0ab6
bcam1 <111377797+bcam1@users.noreply.github.com>
2023-06-26 07:02:14 -0500

    Added release notes for OpCon 22.5.0 (#198)

--- sidebars.js
+++ sidebars.js
@@ -554,7 +554,6 @@ module.exports = {
                     "Files/UI/Solution-Manager/Library/Logs/List-Log-Files",
                     "Files/UI/Solution-Manager/Library/Logs/List-Archive-Files",
                     "Files/UI/Solution-Manager/Library/Logs/Log-File-Details",
-
                   ],
                 },
               ],
@@ -761,7 +760,7 @@ module.exports = {
     {
       type: "link",
       label: "REST API Docs",
-      href: "https://help.smatechnologies.com/opcon/core/api/22-4.html",
+      href: "https://help.smatechnologies.com/opcon/core/api/22-5.html",
     },
   ],
 };
