68ad4bf39d4c1a0a0aeb34f37ea95b5784e90882
Eric <eliao@smatechnologies.com>
2023-07-07 14:08:37 -0400

    Added docs for resetting server options

--- sidebars.js
+++ sidebars.js
@@ -544,6 +544,7 @@ module.exports = {
                     "Files/UI/Solution-Manager/Library/ServerOptions/Managing-General-Settings",
                     "Files/UI/Solution-Manager/Library/ServerOptions/Managing-Logging-Settings",
                     "Files/UI/Solution-Manager/Library/ServerOptions/Managing-Notification-Settings",
+                    "Files/UI/Solution-Manager/Library/ServerOptions/Reset-Settings",
                   ],
                 },
                 {
