1267b267f23f7a66dc8c2bbfafc3adc425ec0455
Daniel Kiss <dkiss@smatechnologies.com>
2023-12-07 17:02:18 -0800

    add master jobs setup

---
+++ docs/Files/UI/Solution-Manager/Library/Working-With-Library/Working-With-Master-Jobs.md
@@ -0,0 +1,11 @@
+---
+lang: en-us
+title: Working with Master Jobs TBD
+viewport: width=device-width, initial-scale=1.0
+---
+
+# Overview
+
+The Master Jobs tab allows you to perform functions for managing the master jobs.
+
+![Working With master jobs](../../../../../Resources/Images/SM/Library/WorkingWithLibrary/WorkingWithMasterJobs.png "Threshold Grid")
--- sidebars.js
+++ sidebars.js
@@ -439,6 +439,7 @@ module.exports = {
               items: [
                 'Files/UI/Solution-Manager/Library/Working-With-Library/Working-With-Library',
                 'Files/UI/Solution-Manager/Library/Working-With-Library/Working-With-Global-Properties',
+                'Files/UI/Solution-Manager/Library/Working-With-Library/Working-With-Master-Jobs',
                 'Files/UI/Solution-Manager/Library/Resources/Resources',
                 'Files/UI/Solution-Manager/Library/Thresholds/Thresholds',
                 'Files/UI/Solution-Manager/Library/AccessCodes/AccessCodes',
