df5453efb548cd7f1ca79b66195e84cfdfccb712
Brian Cam <bcam@smatechnologies.com>
2024-11-21 22:01:13 -0600

    Updated API docs file name for 24.3.3

--- sidebars.js
+++ sidebars.js
@@ -910,7 +910,7 @@ module.exports = {
     {
       type: "link",
       label: "REST API Docs",
-      href: "https://help.smatechnologies.com/opcon/core/api/24-2.html",
+      href: "https://help.smatechnologies.com/opcon/core/api/24-3.html",
     },
   ],
 };
