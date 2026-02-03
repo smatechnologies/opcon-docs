a71531220d566733498c47fd9694c17b47128d2f
Roy Weesner <rweesner@smatechnologies.com>
2026-02-03 10:49:21 -0600

    Release 26.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,10 +1,9 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-const VersionsArchived = require("./versionsArchived.json")
 module.exports = {
   title: "SMA Technologies Help",
   tagline: "OpCon",
   url: "https://help.smatechnologies.com",
-  baseUrl: "/opcon/core/",
+  baseUrl: "/opcon/core/v26.0/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
@@ -18,19 +17,7 @@ module.exports = {
         src: "img/logo.svg",
         href: "https://help.smatechnologies.com",
       },
-      items: [
-        {
-          type: "docsVersionDropdown",
-          position: "right",
-          versions: {
-            current: {label: 'OpCon'},
-            '25.0': {label: '25.0'},
-            '23.0': {label: '23.0'},
-            '22.0': {label: '22.0'},
-            '21.0': {label: '21.0'},
-          },
-        },
-      ],
+      items: [],
     },
     footer: {
       style: "dark",
@@ -45,33 +32,7 @@ module.exports = {
           breadcrumbs: false,
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/smatechnologies/opcon-docs/blob/main",
-          lastVersion: "current",
-          versions: {
-            current: {
-              label: "current",
-            },
-            "25.0": {
-              label: "25.0",
-              path: "v25.0",
-              banner: "none",
-            },
-            "23.0": {
-              label: "23.0",
-              path: "v23.0",
-              banner: "none",
-            },
-            "22.0": {
-              label: "22.0",
-              path: "v22.0",
-              banner: "none",
-            },
-            "21.0": {
-              label: "21.0",
-              path: "v21.0",
-              banner: "none",
-            },
-          },
+          editUrl: "https://github.com/smatechnologies/opcon-docs/blob/releases/26.0",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
--- versioned_docs/version-23.0/Files/UI/Enterprise-Manager/Granting-Privileges-to-Roles.md
+++
@@ -1,4 +0,0 @@
-# Granting Privileges to Roles
-
-Refer to the [Managing Privileges](Managing-Privileges.md)
- section to grant the specific type of privileges to a Role.
