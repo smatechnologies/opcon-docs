edbadcc345630f2240a736ce9a7ba34c8b40e6e5
bcam1 <111377797+bcam1@users.noreply.github.com>
2026-02-23 14:35:05 -0600

    Updated version in API link (#441)

--- sidebars.js
+++ sidebars.js
@@ -924,7 +924,7 @@ module.exports = {
     {
       type: "link",
       label: "REST API Docs",
-      href: "https://help.smatechnologies.com/opcon/core/api/25-1.html",
+      href: "https://help.smatechnologies.com/opcon/core/api/26-0.html",
     },
   ],
 };
